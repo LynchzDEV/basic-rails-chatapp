1b5357a4ad62dc9a466211df96746dd3ea6f6757
LynchzDEV <opmok48@gmail.com>
2025-03-12 16:37:28 +0700

    fixing update in real time

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,29 @@ class Message < ApplicationRecord
 
   validates :content, presence: true
 
-  after_create_commit -> { broadcast_append_to room }
-  after_update_commit -> { broadcast_replace_to room }
+  after_create_commit :broadcast_creation
+  after_update_commit :broadcast_update
   after_destroy_commit -> { broadcast_remove_to room }
+
+  private
+
+  def broadcast_creation
+
+    channel = "room_#{room.id}"
+    Turbo::StreamsChannel.broadcast_append_to(
+      channel,
+      target: "room_#{room.id}_messages_list",
+      partial: "messages/created_message",
+      locals: { message: self }
+    )
+  end
+
+  def broadcast_update
+    Turbo::StreamsChannel.broadcast_replace_to(
+      room,
+      target: "message_#{id}",
+      partial: "messages/message_content",
+      locals: { message: self }
+    )
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
   validates :name, presence: true
   has_many :messages, dependent: :destroy
+
+  after_create_commit -> { broadcast_append_to "rooms" }
+  after_update_commit -> { broadcast_replace_to "rooms" }
+  after_destroy_commit -> { broadcast_remove_to "rooms" }
 end
